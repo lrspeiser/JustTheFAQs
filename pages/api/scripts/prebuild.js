fdc89f981fd15e65e8a1b04bb4491af1a1ca8aff
Leonard Speiser <leonard@horizon3.net>
2025-01-15 20:06:25 +0000

    putting script back in

--- pages/api/scripts/prebuild.js
+++ pages/api/scripts/prebuild.js
@@ -1,7 +1,9 @@
+// pages/api/scripts/prebuild.js
 import fs from 'fs';
 import path from 'path';
 
-const dir = path.join(process.cwd(), 'public/data/faqs');
+// Update the directory to the new path
+const dir = path.join(process.cwd(), 'data/faqs'); // Adjusted path
 
 if (!fs.existsSync(dir)) {
   console.log('[Prebuild] Creating missing directory:', dir);
