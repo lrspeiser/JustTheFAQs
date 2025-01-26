57ae0f99f5a90610c5cb4cd4a3835c26787327db
Leonard Speiser <leonard@horizon3.net>
2025-01-26 04:49:46 +0000

    fix file path

--- pages/api/util/fetch-and-generate.js
+++ pages/api/util/fetch-and-generate.js
@@ -1,4 +1,4 @@
-import { main } from "../../scripts/fetchAndGenerate";
+import { main } from "../scripts/fetchAndGenerate.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
